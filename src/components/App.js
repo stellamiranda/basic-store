fec50223cc03afecb5c73e85deabcca038e05837
Stella Miranda <stella.miranda@unbounce.com>
2020-03-07 14:58:58 -0800

    Add router and App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,17 @@
 import React from "react";
 import Header from "./Header"
+import Order from "./Order"
+import Inventory from "./Inventory"
 
 class App extends React.Component {
   render(){
     return(
       <div className="catch-of-the-day">
         <div className="menu">
-          <Header />
+          <Header tagline="Fresh seafood market"/>
         </div>
-
+        <Order></Order>
+        <Inventory></Inventory>
       </div>
     );
   }
